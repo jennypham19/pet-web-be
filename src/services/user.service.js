44f4921b6b7107ba5376d0d5e0e3e4351552f60b
ngocanh <ngocanh.pham19042001@gmail.com>
2026-03-24 07:36:55 +0700

    pet=be-24032026-7:36

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,6 +3,7 @@ const { User } = require('../models');
 const { StatusCodes } = require('http-status-codes');
 const ApiError = require('../utils/ApiError');
 const bcrypt = require('bcryptjs');
+const { Op } = require('sequelize');
 
 // Lấy chi tiết user theo id
 const getUserById = async(id) => {
@@ -40,6 +41,67 @@ const createAccount = async(accountBody) => {
     }
 }
 
+// Lấy danh sách tài khoản
+const queryAccounts = async(queryOptions) => {
+    try {
+        const { page, limit, searchTerm } = queryOptions;
+        const offset = (page - 1) * limit;
+        const whereClause = {};
+        if(searchTerm){
+            whereClause[Op.or] = [
+                { name: { [Op.iLike]: `%${searchTerm}%` }},
+                { account: { [Op.iLike]: `%${searchTerm}%` }},
+                { phone: { [Op.iLike]: `%${searchTerm}%` }},
+            ]
+        }
+
+        const { count, rows: accountsDB } = await User.findAndCountAll({
+            where: whereClause,
+            limit,
+            offset,
+            order: [[ 'createdAt', 'DESC']]
+        })
+
+        if(accountsDB.length === 0) {
+            throw new ApiError(StatusCodes.NOT_FOUND, 'Không tồn tại bản ghi nào.')
+        }
+        const totalPages = Math.ceil(count/limit);
+        const accounts = accountsDB.map((account) => {
+            const newAccount = account.toJSON();
+            return {
+                id: newAccount.id,
+                name: newAccount.name,
+                account: newAccount.account,
+                role: newAccount.role,
+                gender: newAccount.gender ? newAccount.gender : null,
+                position: newAccount.position ? newAccount.position : null,
+                title: newAccount.title ? newAccount : null,
+                dob: newAccount.dob ? newAccount.dob : null,
+                cccd: newAccount.cccd ? newAccount.cccd : null,
+                email: newAccount.email ? newAccount.email : null,
+                phone: newAccount.phone ? newAccount.phone : null,
+                address: newAccount.address ? newAccount.address : null,
+                isActived: newAccount.is_actived,
+                isDefaultType: newAccount.is_default_type,
+                isReset: newAccount.is_reset,
+                isDeleted: newAccount.is_deleted,
+                avatarUrl: newAccount.avatar_url ? newAccount.avatar_url : null,
+                createdAt: newAccount.createdAt,
+                updatedAt: newAccount.updatedAt
+            }
+        })
+        return {
+            data: accounts,
+            totalPages,
+            currentPage: page,
+            total: count
+        }
+    } catch (error) {
+        throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, "Đã có lỗi xảy ra: " + error.message)
+    }
+}
+
 module.exports = {
     createAccount,
+    queryAccounts
 }
